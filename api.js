e3c61c4d77bbc7be46474ebebc5ff309f2ebb200
Sprachgefuhl <benjsbroja@gmail.com>
2026-07-15 13:45:05 +1000

    first commit

---
+++ api.js
@@ -0,0 +1,34 @@
+const { chromium } = require('playwright');
+const cheerio = require('cheerio');
+
+async function headlessScrape() {
+  const browser = await chromium.launch({
+    headless: true,
+    // slowMo: 100
+  });
+
+  const context = await browser.newContext({
+    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36',
+    // viewport: { width: 1280, height: 720 },
+  });
+
+  const page = await context.newPage();
+
+  try {
+    await page.goto(`https://www.jw.org/en/whats-new/`, { waitUntil: 'networkidle' });
+
+    const html = await page.content();
+    const $ = cheerio.load(html);
+    const articleImages = $('.whatsNewItems img').map((i, el) => $(el).attr('src')).get();
+    const articleLinks = $('.whatsNewItems .syn-img a').map((i, el) => $(el).attr('href')).get();
+
+    return { images: articleImages, links: articleLinks };
+
+  } catch (error) {
+    console.error('❌ Failed:', error.message);
+  } finally {
+    await browser.close();
+  }
+}
+
+module.exports = { headlessScrape }
---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const express = require('express')
+const app = express();
+const { headlessScrape } = require('./api');
+
+app.get('/api', async (req, res) => {
+  const apiKey = req.query.key;
+	if (!apiKey || apiKey !== process.env.API_KEY) return res.status(401).json({ error: 'unauthorized' });
+
+  const data = await headlessScrape();
+	return res.status(200).json({ success: true, data: data });
+});
+
+app.listen(process.env.PORT || 3000);
